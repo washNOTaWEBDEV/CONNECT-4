f7b76d2743df1b24f1a806c3cbc3a9c8f1f9aa2c
wash <91541609+washNOTaWEBDEV@users.noreply.github.com>
2024-04-15 00:35:10 +1000

    Update resetgame.py

--- resetgame.py
+++ resetgame.py
@@ -1,11 +1,11 @@
-from misc import *
+import misc
 from screendisplay import *
 from hover_columns import play_again_rect
 
 def end_of_game():
-    if winner == 1:
+    if misc.winner == 1:
         end_text = "試合終了。The reds have won!"
-    elif winner == -1:
+    elif misc.winner == -1:
         end_text = "試合終了。The yellows have won!"
     else:
         end_text = "なんてこった！drawなのだ！"
@@ -16,12 +16,13 @@ def end_of_game():
     screen.blit(ending_text_bottom, center_ending_text_bottom)
 
 def ask_to_play_again():
-    if winner == 1:
-        play_again_text = "red ready the red red reed rawr"
-    elif winner == -1:
-        play_again_text = "ye yet yoo yada yuck it's you"
-    else:
-        play_again_text = "つまんないよね、このゲーム"
+    play_again_text = "Click to play again!"
+    # if misc.winner == 1:
+    #     play_again_text = "red ready the red red reed rawr"
+    # elif misc.winner == -1:
+    #     play_again_text = "ye yet yoo yada yuck it's you"
+    # else:
+    #     play_again_text = "つまんないよね、このゲーム"
 
     play_again_text = font.render(play_again_text, False, (225, 225, 225))
     center_play_again_text = play_again_text.get_rect()
